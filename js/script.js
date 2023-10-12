c720c7ccf889f11ba79ddc81421e4529f57283fb
nidhinishad21 <nidhinishad21@gmail.com>
2023-10-11 19:31:03 -0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,510 @@
+
+let users = [
+    {
+        "name" : "iboya vat",
+        "image" : "https://randomuser.me/api/portraits/thumb/women/67.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "aapo niskanen",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/34.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "zilda moreira",
+        "image" : "https://randomuser.me/api/portraits/thumb/women/38.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "lilou le gall",
+        "image" : "https://randomuser.me/api/portraits/thumb/women/23.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "lucy hall",
+        "image" : "https://randomuser.me/api/portraits/thumb/women/24.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "sara alves",
+        "image" : "https://randomuser.me/api/portraits/thumb/women/14.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "ramon macrae",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/49.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "connor taylor",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/15.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "aymeric morel",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/11.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "lorenz otto",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/49.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "karl williamson",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/29.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "ouassim heering",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/34.jpg",
+        "joined" : "07/15/15"
+    },
+    {
+        "name" : "roberto molina",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/91.jpg",
+        "joined" : "06/03/15"
+    },
+    {
+        "name" : "jordan hubert",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/28.jpg",
+        "joined" : "06/03/15"
+    },
+    {
+        "name" : "melvin baker",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/29.jpg",
+        "joined" : "09/18/14"
+    },
+    {
+        "name" : "everett gordon",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/26.jpg",
+        "joined" : "06/17/15"
+    },
+    {
+        "name" : "aiden ma",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/82.jpg",
+        "joined" : "07/18/12"
+    },
+    {
+        "name" : "florent gerard",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/62.jpg",
+        "joined" : "02/12/13"
+    },
+    {
+        "name" : "amber chen",
+        "image" : "https://randomuser.me/api/portraits/thumb/women/96.jpg",
+        "joined" : "07/12/12"
+    },
+    {
+        "name" : "alexandra davies",
+        "image" : "https://randomuser.me/api/portraits/thumb/women/9.jpg",
+        "joined" : "05/11/13"
+    },
+    {
+        "name" : "sergio cole",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/57.jpg",
+        "joined" : "02/17/15"
+    },
+    {
+        "name" : "edgar dixon",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/98.jpg",
+        "joined" : "06/17/11"
+    },
+    {
+        "name" : "kirk myers",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/94.jpg",
+        "joined" : "09/17/15"
+    },
+    {
+        "name" : "ani hesseling",
+        "image" : "https://randomuser.me/api/portraits/thumb/women/28.jpg",
+        "joined" : "08/14/16"
+    },
+    {
+        "name" : "victoire bonnet",
+        "image" : "https://randomuser.me/api/portraits/thumb/women/24.jpg",
+        "joined" : "05/13/16"
+    },
+    {
+        "name" : "marcos morales",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/44.jpg",
+        "joined" : "01/12/12"
+    },
+    {
+        "name" : "nils neumann",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/75.jpg",
+        "joined" : "03/11/12"
+    },
+    {
+        "name" : "emily harrison",
+        "image" : "https://randomuser.me/api/portraits/thumb/women/86.jpg",
+        "joined" : "07/18/15"
+    },
+    {
+        "name" : "matthew fortin",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/41.jpg",
+        "joined" : "03/18/15"
+    },
+    {
+        "name" : "charlotte steward",
+        "image" : "https://randomuser.me/api/portraits/thumb/women/32.jpg",
+        "joined" : "02/18/11"
+    },
+    {
+        "name" : "marceau rodriguez",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/3.jpg",
+        "joined" : "07/13/14"
+    },
+    {
+        "name" : "hudson anderson",
+        "image" : "https://randomuser.me/api/portraits/thumb/men/91.jpg",
+        "joined" : "09/12/15"
+    },
+
+    {
+        "name": "Shubhangi Prabhakaran",
+        "image": "https://randomuser.me/api/portraits/women/23.jpg",
+        "joined": "2022/10/04"
+    },
+    {
+        "name": "\u0639\u0631\u0634\u064a\u0627 \u0642\u0627\u0633\u0645\u06cc",
+        "image": "https://randomuser.me/api/portraits/men/57.jpg",
+        "joined": "2022/04/26"
+    },
+    {
+        "name": "Terrance Lopez",
+        "image": "https://randomuser.me/api/portraits/men/6.jpg",
+        "joined": "2023/04/22"
+    },
+    {
+        "name": "Victor Olsen",
+        "image": "https://randomuser.me/api/portraits/men/94.jpg",
+        "joined": "2022/11/30"
+    },
+    {
+        "name": "Kleydianne Cardoso",
+        "image": "https://randomuser.me/api/portraits/women/62.jpg",
+        "joined": "2022/06/27"
+    },
+    {
+        "name": "Sarmat Slivinskiy",
+        "image": "https://randomuser.me/api/portraits/men/8.jpg",
+        "joined": "2022/08/03"
+    },
+    {
+        "name": "L\u00e6rke Petersen",
+        "image": "https://randomuser.me/api/portraits/women/27.jpg",
+        "joined": "2022/03/19"
+    },
+    {
+        "name": "Vanessa Gregory",
+        "image": "https://randomuser.me/api/portraits/women/90.jpg",
+        "joined": "2022/09/21"
+    },
+    {
+        "name": "Carolina Contreras",
+        "image": "https://randomuser.me/api/portraits/women/56.jpg",
+        "joined": "2019/08/19"
+    },
+    {
+        "name": "Viktorija Te\u0161i",
+        "image": "https://randomuser.me/api/portraits/women/64.jpg",
+        "joined": "2021/09/21"
+    },
+    {
+        "name": "Roger May",
+        "image": "https://randomuser.me/api/portraits/men/60.jpg",
+        "joined": "2021/03/27"
+    },
+    {
+        "name": "Arsen Ieroshenko",
+        "image": "https://randomuser.me/api/portraits/men/83.jpg",
+        "joined": "2022/05/21"
+    },
+    {
+        "name": "Emre \u00d6ztonga",
+        "image": "https://randomuser.me/api/portraits/men/51.jpg",
+        "joined": "2021/07/04"
+    },
+    {
+        "name": "Evdokija \u017diki",
+        "image": "https://randomuser.me/api/portraits/women/1.jpg",
+        "joined": "2020/07/03"
+    },
+    {
+        "name": "Ljubica Vidakovi",
+        "image": "https://randomuser.me/api/portraits/women/85.jpg",
+        "joined": "2020/11/17"
+    },
+    {
+        "name": "Bill Bradley",
+        "image": "https://randomuser.me/api/portraits/men/11.jpg",
+        "joined": "2019/12/04"
+    },
+    {
+        "name": "Eleonora Ninkovi",
+        "image": "https://randomuser.me/api/portraits/women/96.jpg",
+        "joined": "2023/08/30"
+    },
+    {
+        "name": "Noem Castellanos",
+        "image": "https://randomuser.me/api/portraits/women/58.jpg",
+        "joined": "2023/08/17"
+    },
+    {
+        "name": "Leo Gagnon",
+        "image": "https://randomuser.me/api/portraits/men/70.jpg",
+        "joined": "2019/09/20"
+    },
+    {
+        "name": "Mario Shaw",
+        "image": "https://randomuser.me/api/portraits/men/87.jpg",
+        "joined": "2022/06/12"
+    },
+    {
+        "name": "Gilberte Rolland",
+        "image": "https://randomuser.me/api/portraits/women/15.jpg",
+        "joined": "2021/05/28"
+    },
+    {
+        "name": "Lee Oliver",
+        "image": "https://randomuser.me/api/portraits/men/60.jpg",
+        "joined": "2023/05/15"
+    },
+    {
+        "name": "Alberte Mller",
+        "image": "https://randomuser.me/api/portraits/women/13.jpg",
+        "joined": "2022/07/12"
+    },
+    {
+        "name": "Laura Caballero",
+        "image": "https://randomuser.me/api/portraits/women/51.jpg",
+        "joined": "2022/12/31"
+    },
+    {
+        "name": "Esparta Soto",
+        "image": "https://randomuser.me/api/portraits/women/80.jpg",
+        "joined": "2021/12/04"
+    },
+    {
+        "name": "Anabel Miranda",
+        "image": "https://randomuser.me/api/portraits/women/56.jpg",
+        "joined": "2020/10/08"
+    },
+    {
+        "name": "Alex Fraser",
+        "image": "https://randomuser.me/api/portraits/men/1.jpg",
+        "joined": "2022/08/09"
+    },
+    {
+        "name": "Dawn Beck",
+        "image": "https://randomuser.me/api/portraits/women/2.jpg",
+        "joined": "2022/08/23"
+    },
+    {
+        "name": "Catalina Navarrete",
+        "image": "https://randomuser.me/api/portraits/women/25.jpg",
+        "joined": "2023/04/03"
+    },
+    {
+        "name": "Valeriya Maksimchuk",
+        "image": "https://randomuser.me/api/portraits/women/20.jpg",
+        "joined": "2020/11/24"
+    },
+    {
+        "name": "Magdalena Angulo",
+        "image": "https://randomuser.me/api/portraits/women/42.jpg",
+        "joined": "2021/04/24"
+    },
+    {
+        "name": "Mila Borojevi",
+        "image": "https://randomuser.me/api/portraits/women/59.jpg",
+        "joined": "2019/03/02"
+    },
+    {
+        "name": "Laurentius Appelo",
+        "image": "https://randomuser.me/api/portraits/men/52.jpg",
+        "joined": "2019/08/09"
+    },
+    {
+        "name": "Monisha Chatterjee",
+        "image": "https://randomuser.me/api/portraits/women/22.jpg",
+        "joined": "2021/09/01"
+    },
+    {
+        "name": "Hrisa Ryabenko",
+        "image": "https://randomuser.me/api/portraits/women/44.jpg",
+        "joined": "2022/05/13"
+    },
+    {
+        "name": "Sinooka Garmatiy",
+        "image": "https://randomuser.me/api/portraits/women/7.jpg",
+        "joined": "2023/04/18"
+    },
+    {
+        "name": "Antonia Guillaume",
+        "image": "https://randomuser.me/api/portraits/women/49.jpg",
+        "joined": "2023/09/10"
+    },
+    {
+        "name": "Adolfo Ibez",
+        "image": "https://randomuser.me/api/portraits/men/25.jpg",
+        "joined": "2022/10/25"
+    },
+    {
+        "name": "Carter Brown",
+        "image": "https://randomuser.me/api/portraits/men/33.jpg",
+        "joined": "2020/04/15"
+    },
+    {
+        "name": "Erhard Roussel",
+        "image": "https://randomuser.me/api/portraits/men/29.jpg",
+        "joined": "2023/04/18"
+    },
+    {
+        "name": "Arquimedes Barbosa",
+        "image": "https://randomuser.me/api/portraits/men/24.jpg",
+        "joined": "2020/12/02"
+    },
+    {
+        "name": "Hugh Gilbert",
+        "image": "https://randomuser.me/api/portraits/men/99.jpg",
+        "joined": "2021/10/20"
+    },
+    {
+        "name": "Larisa Gocko",
+        "image": "https://randomuser.me/api/portraits/women/61.jpg",
+        "joined": "2019/03/28"
+    },
+    {
+        "name": "Claude Collins",
+        "image": "https://randomuser.me/api/portraits/men/18.jpg",
+        "joined": "2020/12/04"
+    },
+    {
+        "name": "Ceylan Yetkiner",
+        "image": "https://randomuser.me/api/portraits/women/34.jpg",
+        "joined": "2019/06/11"
+    },
+    {
+        "name": "Nooa Ruona",
+        "image": "https://randomuser.me/api/portraits/men/74.jpg",
+        "joined": "2020/10/05"
+    },
+    {
+        "name": "Laura Wilson",
+        "image": "https://randomuser.me/api/portraits/women/11.jpg",
+        "joined": "2019/01/18"
+    },
+    {
+        "name": "Magdalina Miroshnichenko",
+        "image": "https://randomuser.me/api/portraits/women/15.jpg",
+        "joined": "2022/12/15"
+    },
+    {
+        "name": "Ins Martin",
+        "image": "https://randomuser.me/api/portraits/women/33.jpg",
+        "joined": "2021/06/26"
+    },
+    {
+        "name": "Batrice Gauthier",
+        "image": "https://randomuser.me/api/portraits/women/59.jpg",
+        "joined": "2021/01/03"
+    },
+    {
+        "name": "Viivi Pollari",
+        "image": "https://randomuser.me/api/portraits/women/85.jpg",
+        "joined": "2023/03/01"
+    },
+    {
+        "name": "Akshay Patil",
+        "image": "https://randomuser.me/api/portraits/men/97.jpg",
+        "joined": "2018/11/07"
+    },
+    {
+        "name": "Ela Dumont",
+        "image": "https://randomuser.me/api/portraits/women/39.jpg",
+        "joined": "2020/06/01"
+    }
+
+
+]
+
+let usersToRemove = Math.floor(Math.random() * users.length) + 1;
+
+users.splice(0, usersToRemove);
+
+
+initiate();
+
+function initiate() {
+
+    document.getElementsByClassName("page-header")[0].getElementsByTagName("h3")[0].innerText = "Total: " + users.length;
+
+     // Find the number of pages and then create the pages
+     let no_of_pages = users.length / 10;
+
+     let page_div = document.getElementsByClassName("pagination")[0];
+ 
+     for (var i = 0; i < no_of_pages; i++) {
+         let page = document.createElement("a");
+         page.setAttribute("href", "#");
+         page.setAttribute("onClick", "addItems("+ i +")");
+         page.innerText = i + 1;
+ 
+         page_div.appendChild(page);
+     }
+
+     addItems(0);
+}
+
+
+function addItems(pageNo) {
+    let startIndex = pageNo * 10;
+    let endIndex = startIndex + 10;
+
+    let contact_div = document.getElementsByClassName("contact-list")[0];
+    contact_div.innerHTML = "";
+
+    for (var i = startIndex; i < endIndex; i++) {
+        let li_ele = document.createElement('li');
+        li_ele.setAttribute("class", "contact-item cf");
+
+        let div_ele = document.createElement('div');
+        div_ele.setAttribute("class", "contact-details");
+
+        let img_ele = document.createElement('img');
+        img_ele.setAttribute("class", "avatar");
+        img_ele.setAttribute("src", users[i].image);
+
+        let h3_ele = document.createElement('h3');
+        h3_ele.innerText = users[i].name;
+
+        let span_ele = document.createElement('span');
+        span_ele.setAttribute("class", "email");
+        span_ele.innerText = users[i].name + "@example.com";
+
+        div_ele.appendChild(img_ele);
+        div_ele.appendChild(h3_ele);
+        div_ele.appendChild(span_ele);
+
+        
+
+
+        let div2_ele = document.createElement('div');
+        div2_ele.setAttribute("class", "joined-details");
+
+        let span2_ele = document.createElement('span');
+        span2_ele.setAttribute("class", "date");
+        span2_ele.innerText = users[i].joined;
+
+        div2_ele.appendChild(span2_ele);
+
+        li_ele.appendChild(div_ele);
+        li_ele.appendChild(div2_ele);
+
+        
+        contact_div.appendChild(li_ele);
+        
+    }
+}
